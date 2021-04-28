12fe201467ec10adaffc75c8337762d60c3372b2
SapnaGulati <sapnagulati9990@gmail.com>
2021-04-28 16:20:33 +0530

    Make bird fly

--- app/src/main/java/com/example/myflappybird/GameView.kt
+++ app/src/main/java/com/example/myflappybird/GameView.kt
@@ -9,17 +9,23 @@ import android.view.View
 import android.view.WindowManager
 
 class GameView(context: Context?) : View(context) {
-    private lateinit var runnable: Runnable
-    private val updateTime: Long = 30
+    private var runnable: Runnable = Runnable {
+        invalidate()
+    }
+    private val updateTime: Long = 50
     private var background: Bitmap
     private var point: Point
     private var windowManager: WindowManager
     private var displayMetrics: DisplayMetrics
     private var height: Int? = null
     private var width: Int? = null
+    private var birdWidth: Int? = null
+    private var birdHeight: Int? = null
     private var rect: Rect
     private var birds: ArrayList<Bitmap> = arrayListOf()
     private var birdFrame = 0
+    private var velocity = 0
+    private var gravity = 3
 
     init {
         birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_0))
@@ -45,29 +51,21 @@ class GameView(context: Context?) : View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(background, null, rect, null)
-        if(birdFrame == 0)
-            birdFrame = 1
-        if(birdFrame == 1)
-            birdFrame = 2
-        if(birdFrame == 2)
-            birdFrame = 3
-        if(birdFrame == 3)
-            birdFrame = 4
-        if(birdFrame == 4)
-            birdFrame = 5
-        if(birdFrame == 5)
-            birdFrame = 6
-        if(birdFrame == 6)
-            birdFrame = 7
-        if(birdFrame == 7)
-            birdFrame = 0
-
-        val birdWidth = (width!! /2) - (birds[0].width/2)
-        val birdHeight = (height!! /2) - (birds[0].height/2)
-
-        canvas?.drawBitmap(birds[birdFrame], birdWidth.toFloat(), birdHeight.toFloat(), null)
-        runnable = Runnable {
-            Handler(Looper.getMainLooper()).postDelayed(runnable, updateTime)
+        birdFrame = when (birdFrame) {
+            0 -> 1
+            1 -> 2
+            2 -> 3
+            3 -> 4
+            4 -> 5
+            5 -> 6
+            6 -> 7
+            else -> 0
         }
+
+        birdWidth = (width!! /2) - (birds[0].width/2)
+        birdHeight = (height!! /2) - (birds[0].height/2)
+
+        canvas?.drawBitmap(birds[birdFrame], birdWidth!!.toFloat(), birdHeight!!.toFloat(), null)
+            Handler(Looper.getMainLooper()).postDelayed(runnable, updateTime)
     }
 }
