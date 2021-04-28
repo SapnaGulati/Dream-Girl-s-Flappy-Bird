19a0b71bd8c58c0c98523567b443a586f1aa1471
SapnaGulati <sapnagulati9990@gmail.com>
2021-04-28 15:52:21 +0530

    Added bird to screen

--- app/src/main/java/com/example/myflappybird/GameView.kt
+++ app/src/main/java/com/example/myflappybird/GameView.kt
@@ -18,11 +18,19 @@ class GameView(context: Context?) : View(context) {
     private var height: Int? = null
     private var width: Int? = null
     private var rect: Rect
+    private var birds: ArrayList<Bitmap> = arrayListOf()
+    private var birdFrame = 0
 
     init {
-        fun run() {
-            invalidate()
-        }
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_0))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_1))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_2))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_3))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_4))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_5))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_6))
+        birds.add(BitmapFactory.decodeResource(resources, R.drawable.frame_7))
+
         background = BitmapFactory.decodeResource(resources, R.drawable.background)
         windowManager = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val display = windowManager.defaultDisplay
@@ -37,6 +45,27 @@ class GameView(context: Context?) : View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(background, null, rect, null)
+        if(birdFrame == 0)
+            birdFrame = 1
+        if(birdFrame == 1)
+            birdFrame = 2
+        if(birdFrame == 2)
+            birdFrame = 3
+        if(birdFrame == 3)
+            birdFrame = 4
+        if(birdFrame == 4)
+            birdFrame = 5
+        if(birdFrame == 5)
+            birdFrame = 6
+        if(birdFrame == 6)
+            birdFrame = 7
+        if(birdFrame == 7)
+            birdFrame = 0
+
+        val birdWidth = (width!! /2) - (birds[0].width/2)
+        val birdHeight = (height!! /2) - (birds[0].height/2)
+
+        canvas?.drawBitmap(birds[birdFrame], birdWidth.toFloat(), birdHeight.toFloat(), null)
         runnable = Runnable {
             Handler(Looper.getMainLooper()).postDelayed(runnable, updateTime)
         }
